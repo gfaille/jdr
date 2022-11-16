ac2dc9e417c6678d9f49ca1a9fa06467a76c090d
Skykru59 <114058603+Skykru59@users.noreply.github.com>
2022-11-16 11:35:33 +0100

    creation et dimension modifie de la map

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+ # fenêtre et image
+longueur = 1000
+largeur = 1000
+fenetre = pygame.display.set_mode((longueur, largeur))
+fond = pygame.image.load("images/test.png").convert_alpha()
+redimensionner_image = (1500, 1500)
+fond = pygame.transform.scale(fond, redimensionner_image)
+fenetre.blit(fond, (0, 0))
+pygame.display.flip()
+pygame.time.delay(5000)
