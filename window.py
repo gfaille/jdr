eaaa9aa250d5511bc78a3fc74d66acbbfffca99f
gfaille <zerteducon@gmail.com>
2022-11-21 11:52:14 +0100

    ajout d'une fonctionne pour effacer l'ecran

--- window.py
+++ window.py
@@ -6,4 +6,10 @@ pygame.init()
 # affichage de la fenêtre pygame en 720p
 longueur = 1280
 largeur = 720
-fenetre = pygame.display.set_mode((longueur, largeur))
+flags = pygame.SHOWN
+fenetre = pygame.display.set_mode((longueur, largeur), flags)
+
+def effacer_ecran () :
+    
+    # éfface l'ecran
+    fenetre.fill((0, 0, 0))
