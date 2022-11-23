1f8c805805e6c85c86b876d5e199e631c9f750ef
gfaille <zerteducon@gmail.com>
2022-11-23 15:02:27 +0100

    fonctionnalité ajouter et fonctionnel dans le menu des options, permet désormais de changer de résolution et aussi de passer en plein-écran ou en fenêtré. Les résolution disponible sont : 720X480, 800X600, 1024X768, 1280X720, 1680X1050 et 1920X 1280 qui corresponde à ntsc, svga, xga, hd, wsxga, fhd.

--- test.py
+++ test.py
@@ -9,42 +9,6 @@ largeur = 720
 flags = pygame.SHOWN
 fenetre = pygame.display.set_mode((longueur, largeur), flags)
 
-
-""" # dessiner du texte sur une nouvelle Surface créer un rectangle pour la surface centrée à une position donnée du menu des options
-text_resolution = police.render("Affichage", 1, (255, 255, 255))
-text_touche_clavier = police.render("Raccourcie clavier", 1, (255, 255, 255))
-text_son = police.render("Son", 1, (255, 255, 255))
-text_retour = police.render("Retour", 1, (255, 255, 255))
-
-bouton_resolution = text_resolution.get_rect(center=(window.longueur*0.20, window.largeur/1.25))
-bouton_son = text_son.get_rect(center=(window.longueur*0.40, window.largeur/1.25))
-bouton_raccourci = text_touche_clavier.get_rect(center=(window.longueur*0.60, window.largeur/1.25))
-bouton_retour = text_retour.get_rect(center=(window.longueur*0.80, window.largeur/1.25))
-
-# initialisation des options d'affichage (rendu + position)
-text_resolution_ntsc = police.render("720 X 480", 1, (255, 255, 255))
-text_resolution_svga = police.render("800 X 600", 1, (255, 255, 255))
-text_resolution_xga = police.render("1024 X 768", 1, (255, 255, 255))
-text_resolution_hd = police.render("1280 X 720", 1, (255, 255, 255))
-text_resolution_wsxga = police.render("1680 X 1050", 1, (255, 255, 255))
-text_resolution_fhd = police.render("1920 X 1280", 1, (255, 255, 255))
-#######################################################################
-bouton_resolution_ntsc = text_resolution_ntsc.get_rect(center=(window.longueur*0.20, window.largeur*0.2))
-bouton_resolution_svga = text_resolution_svga.get_rect(center=(window.longueur*0.20, window.largeur*0.2))
-bouton_resolution_xga = text_resolution_xga.get_rect(center=(window.longueur*0.20, window.largeur*0.2))
-bouton_resolution_hd = text_resolution_hd.get_rect(center=(window.longueur*0.20, window.largeur*0.2))
-bouton_resolution_wsxga = text_resolution_wsxga.get_rect(center=(window.longueur*0.20, window.largeur*0.2))
-bouton_resolution_fhd = text_resolution_fhd.get_rect(center=(window.longueur*0.20, window.largeur*0.2)) """
-
-""" # creation du selecteur de menu
-select = pygame.image.load("sprites/arrow.png")
-select_width = select.get_width()
-select_height = select.get_height()
-select = pygame.transform.smoothscale(select, (select_height/2, select_width/2)) """
-
-##################################################################################################################################
-
-
 def dessiner_texte (text, size, background, x, y) :
     """ fonction qui permet de dessiné du texte et le centrer dans un arriere plan (il peut être transparant, une couleur unit)
         puis l'affiche dans la fenêtre.
@@ -63,9 +27,12 @@ def dessiner_texte (text, size, background, x, y) :
     fenetre.blit(texte, buton_texte)
     return rect_bouton # retourne les coordonnées x et y du rectangle et sa dimension (largeur, hauteur)
 
+
 continuer = True
 game = False # variable pour le jeux (False on est dans le menu, True on est dans le jeux)
 menu = "menu" # variable pour le menu, permet de naviger dans divers menu et sous-menu
+option_menu = "" # variable pour naviguer dans les sous-menu 
+is_fullscreen = False # variable pour déterminé si on est en plein écran
 
 # boucle principal
 while continuer :
@@ -115,7 +82,7 @@ while continuer :
                         continuer = False
 
         elif menu == "options" :
-            
+
             # éfface l'ecran
             fenetre.fill((0, 0, 0))
 
@@ -126,12 +93,84 @@ while continuer :
             bouton_raccourci = dessiner_texte("raccourci clavier", 16, (51, 101, 138), longueur*0.15, largeur*0.40)
             bouton_retour = dessiner_texte("Retour", 16, (51, 101, 138), longueur*0.15, largeur*0.50)
 
+            # affiche le paramètre des options d'affichage
+            if option_menu == "affichage" :
+
+                # éfface l'ecran
+                fenetre.fill((0, 0, 0))
+
+                # affiche les boutons pour les options d'affichage (résolution, taille ecran, ...) et le titre
+                dessiner_texte("Affichage", 32, (0, 0, 0), longueur*0.45, largeur*0.10)
+                dessiner_texte("Résolution", 18, (0, 0, 0), longueur*0.14, largeur*0.20)
+                bouton_resolution_ntsc = dessiner_texte("720 X 480", 16, (51, 101, 138), longueur*0.15, largeur*0.30)
+                bouton_resolution_svga = dessiner_texte("800 X 600", 16, (51, 101, 138), longueur*0.15, largeur*0.40)
+                bouton_resolution_xga = dessiner_texte("1024 X 768", 16, (51, 101, 138), longueur*0.15, largeur*0.50)
+                bouton_resolution_hd = dessiner_texte("1280 X 720", 16, (51, 101, 138), longueur*0.15, largeur*0.60)
+                bouton_resolution_wsxga = dessiner_texte("1680 X 1050", 16, (51, 101, 138), longueur*0.15, largeur*0.70)
+                bouton_resolution_fhd = dessiner_texte("1920 X 1280", 16, (51, 101, 138), longueur*0.15, largeur*0.80)
+                bouton_fullscrenn = dessiner_texte("fullscrenn", 16, (51, 101, 138), longueur*0.50, largeur*0.25)
+
+                for event in pygame.event.get() :
+                    if event.type == pygame.MOUSEBUTTONDOWN :
+                       
+                       # capture les event de la souris dans les options d'affichages
+                        if bouton_resolution_ntsc.collidepoint(event.pos) :
+                            longueur, largeur = 720, 480
+                            fenetre = pygame.display.set_mode((longueur, largeur), flags)
+                        elif bouton_resolution_svga.collidepoint(event.pos) :
+                            longueur, largeur = 800, 600
+                            fenetre = pygame.display.set_mode((longueur, largeur), flags)
+                        elif bouton_resolution_xga.collidepoint(event.pos) :
+                            longueur, largeur = 1024, 768
+                            fenetre = pygame.display.set_mode((longueur, largeur), flags)
+                        elif bouton_resolution_hd.collidepoint(event.pos) :
+                            longueur, largeur = 1280, 720
+                            fenetre = pygame.display.set_mode((longueur, largeur), flags)
+                        elif bouton_resolution_wsxga.collidepoint(event.pos) :
+                            longueur, largeur = 1680, 1050
+                            fenetre = pygame.display.set_mode((longueur, largeur), flags)
+                        elif bouton_resolution_fhd.collidepoint(event.pos) :
+                            longueur, largeur = 1920, 1280
+                            fenetre = pygame.display.set_mode((longueur, largeur), flags)
+                        elif bouton_fullscrenn.collidepoint(event.pos) :
+                            if is_fullscreen == False :
+                                flags = pygame.FULLSCREEN | pygame.SCALED
+                                fenetre = pygame.display.set_mode((longueur, largeur), flags)
+                                is_fullscreen = True
+                            else :
+                                flags = pygame.SHOWN
+                                fenetre = pygame.display.set_mode((longueur, largeur), flags)
+                                is_fullscreen = False
+
+            # affiche les paramètre de son
+            elif option_menu == "son" :
+
+                # éfface l'ecran
+                fenetre.fill((0, 0, 0))
+
+            # affiche les paramètre des raccourci clavier
+            elif option_menu == "raccourci" :
+
+                # éfface l'ecran
+                fenetre.fill((0, 0, 0))
+
             for event in pygame.event.get():
                 
                 if event.type == pygame.MOUSEBUTTONDOWN :
+
+                    # capture les event de la souris dans les options 
                     if bouton_retour.collidepoint(event.pos) :
                         menu = "menu"
 
+                    elif bouton_affichage.collidepoint(event.pos) :
+                        option_menu = "affichage"
+
+                    elif bouton_son.collidepoint(event.pos) :
+                        option_menu = "son"
+
+                    elif bouton_raccourci.collidepoint(event.pos) :
+                        option_menu = "raccourci"
+                         
         pygame.display.flip() # mise à jour total de l'écran
 
 pygame.quit()
