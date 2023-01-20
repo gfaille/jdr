0074bbe2876ad33de9cbd979bb976fec17f2ab0d
gfaille <zerteducon@gmail.com>
2023-01-20 13:41:46 +0100

    import os, pygame, crud puis crée le fichier config.ini s'il n'existe pas et initialise pygame

---
+++ JDR kingdom heart.py
@@ -0,0 +1,10 @@
+import os
+import pygame
+import crud
+
+# créer le fichier config.ini si il n'existe pas dans le dossier
+if not os.path.isfile("config.ini") :
+    crud.creer_fichier_config()
+
+# initialise pygame
+pygame.init()
